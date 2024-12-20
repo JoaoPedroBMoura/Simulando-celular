d3f6bcb1c462b3f46c453ddda247980fba8fdeb6
joaoPedroBM <joaopedrobarcelllosmoura@gmail.com>
2024-12-20 20:45:02 -0300

    fix: Solving challenge

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Celular Nokia: ");
+Nokia nokia = new Nokia("2199999999","Tijolão","91919191919",64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatzapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Celular Iphone: ");
+Iphone iphone = new Iphone("22888888888", "15 Pro Max Plus Delux optimus Prime","7773777737",128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Google Fotos");
